899b734ec0b15928bb36388a8f342ed6e168dec6
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-22 21:47:29 +0500

    41z_last

--- 41z/Program.cs
+++ 41z/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите числа: ");
+int[] array = Console.ReadLine().Split(",").Select(x => int.Parse(x)).ToArray();
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+        count++;
+}
+Console.WriteLine(count);
