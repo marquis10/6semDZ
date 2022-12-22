210aa91fe35009b58b3f661fafea3d80980afba5
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-22 23:32:39 +0500

    dops

--- 43z/Program.cs
+++ 43z/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите координаты 2 прямых k1, b1, k2, b2:");
+int k1 = Convert.ToInt32(Console.ReadLine());
+int b1 = Convert.ToInt32(Console.ReadLine());
+int k2 = Convert.ToInt32(Console.ReadLine());
+int b2 = Convert.ToInt32(Console.ReadLine());
+int x = (b2 - b1) / (k1 - k2), y = 0;
