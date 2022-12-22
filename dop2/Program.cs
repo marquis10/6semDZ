f188cbd5444fd64a8b17b6219a0be553f1b24b31
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-23 01:48:56 +0500

    dop2_l

--- dop2/Program.cs
+++ dop2/Program.cs
@@ -1,28 +1,14 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите координаты вершин треугольника (x1, y1), (x2, y2), (x3, y3): ");
 int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
-int x1 = array[0], y1 = array[1], x2 = array[2], y2 = array[3], x3 = array[4], y3 = array[5];
-
-int AB = Convert.ToInt32(Math.Round(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(x1 - x2, 2)), 2));
-int BC = Convert.ToInt32(Math.Round(Math.Sqrt(Math.Pow(x2 - x3, 2) + Math.Pow(x2 - x3, 2)), 2));
-int AC = Convert.ToInt32(Math.Round(Math.Sqrt(Math.Pow(x1 - x3, 2) + Math.Pow(x1 - x3, 2)), 2));
-int P = Convert.ToInt32((AB+BC+AC)/2);
-int S = Convert.ToInt32(Math.Round(Math.Sqrt(P*(P-AC)*(P-BC)*(P-AC))));
-Console.WriteLine(S);
-
-/*
-Console.Clear();
-Console.WriteLine("Введите координаты вершин треугольника (x1, y1), (x2, y2), (x3, y3): ");
-int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
-int x1 = array[0], y1 = array[1], x2 = array[2], y2 = array[3], x3 = array[4], y3 = array[5];
-Console.WriteLine($"Длина стороны AB = {Math.Round(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(x1 - x2, 2)), 2)}");
-int AB = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Длина стороны BC = {Math.Round(Math.Sqrt(Math.Pow(x2 - x3, 2) + Math.Pow(x2 - x3, 2)), 2)}");
-int BC = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Длина стороны AC = {Math.Round(Math.Sqrt(Math.Pow(x1 - x3, 2) + Math.Pow(x1 - x3, 2)), 2)}");
-int AC = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Произведение полупериметра = {(AB+BC+AC)/2}");
-int P = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Площадь треугольника по формуле Герона = {Math.Round(Math.Sqrt(P*(P-AC)*(P-BC)*(P-AC)))}");
-int S = Convert.ToInt32(Console.ReadLine());
-*/
+int x1 = array[0], y1 = array[1], x2 = array[2], y2 = array[3], x3 = array[4], y3 = array[5], 
+AB = Convert.ToInt32(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2))), 
+BC = Convert.ToInt32(Math.Sqrt(Math.Pow(x2 - x3, 2) + Math.Pow(y2 - y3, 2))), 
+AC = Convert.ToInt32(Math.Sqrt(Math.Pow(x1 - x3, 2) + Math.Pow(y1 - y3, 2))), 
+P = (AB+BC+AC)/2, 
+S = Convert.ToInt32(Math.Sqrt(P*(P-AB)*(P-BC)*(P-AC)));
+Console.WriteLine($"Длина стороны AB = {AB}");
+Console.WriteLine($"Длина стороны BC = {BC}");
+Console.WriteLine($"Длина стороны AC = {AC}");
+Console.WriteLine($"Произведение полупериметра = {P}");
+Console.WriteLine($"Площадь треугольника по формуле Герона = {S}");
