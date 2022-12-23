6d30fbc324059128fc259f7fbec99513bd12ee5e
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-23 14:56:57 +0500

    43_last2

--- 43z/Program.cs
+++ 43z/Program.cs
@@ -5,4 +5,11 @@ double k1 = Convert.ToInt32(Console.ReadLine());
 double b2 = Convert.ToInt32(Console.ReadLine());
 double k2 = Convert.ToInt32(Console.ReadLine());
 double x = 0, y = 0;
-Console.WriteLine($"X = {x = (b2 - b1) / (k1 - k2)}\nY = {y = k1 * x + b1}");
+if (b1 == b2 && k1 == k2)
+    Console.WriteLine("Прямые совпадают");
+else if (k1 == k2)
+    {
+        Console.WriteLine("Прямые параллельны и не пересекутся, т.к. k1 = k2");
+    }
+else
+    Console.WriteLine($"Точка пересечения двух прямых произойдёт в координате: X = {x = (b2 - b1) / (k1 - k2)}\nY = {y = k1 * x + b1}");
