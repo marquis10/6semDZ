bcc2d66eb443cbd398f5dbbcae9536357caf2cc6
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-23 14:35:54 +0500

    43z_last1

--- 43z/Program.cs
+++ 43z/Program.cs
@@ -1,7 +1,8 @@
 ﻿Console.Clear();
-Console.WriteLine("Введите координаты 2 прямых k1, b1, k2, b2:");
-int k1 = Convert.ToInt32(Console.ReadLine());
-int b1 = Convert.ToInt32(Console.ReadLine());
-int k2 = Convert.ToInt32(Console.ReadLine());
-int b2 = Convert.ToInt32(Console.ReadLine());
-int x = (b2 - b1) / (k1 - k2), y = 0;
+Console.WriteLine("Введите координаты 2 прямых b1, k1, b2, k2:");
+double b1 = Convert.ToInt32(Console.ReadLine());
+double k1 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
+double k2 = Convert.ToInt32(Console.ReadLine());
+double x = 0, y = 0;
+Console.WriteLine($"X = {x = (b2 - b1) / (k1 - k2)}\nY = {y = k1 * x + b1}");
